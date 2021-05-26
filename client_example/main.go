a368b2c8465802f6aa732b7fa1b249e2f1569fd1
Eugene Chertikhin <e.chertikhin@crestwavetech.ru>
2021-05-26 23:50:17 +0300

    client example - add test implementation

--- client_example/main.go
+++ client_example/main.go
@@ -1,21 +1,42 @@
 package main
 
 import (
+	"github.com/MajaSuite/mqtt/packet"
 	"github.com/MajaSuite/mqtt/transport"
-	"os"
-	"os/signal"
-	"syscall"
+	"log"
+	"time"
 )
 
 func main() {
-	_, err := transport.Connect("127.0.0.1:1883", "mqttclient", "", "")
+	client, err := transport.Connect("127.0.0.1:1883", "go-mqttclient", 30,"", "")
 	if err != nil {
 		panic(err)
 	}
 
+	s := packet.NewSubscribe()
+	s.Topics = []packet.SubscribePayload{
+		packet.SubscribePayload{Topic: "home/topic", QoS: 0},
+		packet.SubscribePayload{Topic: "home/another", QoS: 0},
+	}
+	client.Subscribe(s)
 
-	finish := make(chan os.Signal, 1)
-	signal.Notify(finish, syscall.SIGINT, syscall.SIGTERM)
-	<-finish
+	u := packet.NewUnSub()
+	u.Topics = []packet.SubscribePayload{
+		packet.SubscribePayload{Topic: "home/another", QoS: 0},
+	}
+	client.Unsubscribe(u)
+
+	p := packet.NewPublish()
+	p.Topic = "home/topic"
+	p.Payload = "{}"
+	client.Publish(p)
+
+	for e := range client.Broker {
+		if e.PacketType == packet.PUBLISH {
+			log.Println("NEW PUBLISH: ", e)
+		}
+	}
+
+	client.Disconnect()
+	time.Sleep(time.Second*2)
 }
-
